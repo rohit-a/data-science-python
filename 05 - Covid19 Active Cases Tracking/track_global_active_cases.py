b14c4dc7f7bb54795765f27bfc797d16b9c751c6
Rohit Anurag <rohitanurag@outlook.com>
2020-05-09 20:24:44 +0530

    Improved data wrangling, Added Initial Data Plotting

--- 05 - Covid19 Active Cases Tracking/track_global_active_cases.py
+++ 05 - Covid19 Active Cases Tracking/track_global_active_cases.py
@@ -20,10 +20,10 @@ files_url = {
             }
 
 
-def get_data(file_path):
+def get_data(case_type, file_path):
     # Reading file from file path and storing in data frame
     file_df = pd.read_csv(file_path)
-
+    
     # Renaming and removing columns
     file_df = file_df.rename(columns={"Country/Region":"Country"}).drop(["Lat","Long"], axis="columns")
 
@@ -31,54 +31,44 @@ def get_data(file_path):
     file_df = file_df.groupby("Country").sum()
 
     # Pivoting down data
-    file_df = file_df.reset_index().melt(id_vars='Country', var_name="Date")
+    file_df = file_df.reset_index().melt(id_vars='Country', var_name="Date", value_name=case_type)
 
     # Changing Date from String to Date type
     file_df['Date'] = pd.to_datetime(file_df['Date'])
-
+    
     return file_df
 
-# Creating a blank data frame
-covid19_data = pd.DataFrame()
+def plot_data(df, columns, x='Date', row='Country', title=""):
+    df = df[columns].reset_index().melt(id_vars=[row, x], var_name='Metric', value_name='Metric Value')
+    g=sns.relplot(x=x, y='Metric Value', col='Metric', data=df, hue=row, kind='line', facet_kws={'sharey':'col'})  
+    g.set_xticklabels(rotation=45)
+    g.fig.suptitle(title, y=1.01)
 
-# Looping through files to create combined data frame
-for name, path in files_url.items():
-    df = get_data(path)
-    df['Type'] = name
-    covid19_data = covid19_data.append(df)
+# Creating data frame with appended Confirmed, Recovered and Deaths data
+covid19_data = pd.concat([get_data(file_type, path) for file_type, path in files_url.items()])
 
-# Pivoting up on type to get single row for each Country, Date
-covid19_data = covid19_data.pivot_table(index=['Country','Date'], columns = 'Type', values = 'value', aggfunc=sum, fill_value=0).reset_index()
+# Creating signle row for Country, Date by aggregating data.
+covid19_data = covid19_data.groupby(['Country','Date']).sum()
 
+# Calculating Active cases and % Cases Active
+covid19_data['Active'] = covid19_data['Confirmed'] - (covid19_data['Deaths'] + covid19_data['Recovered'])
+covid19_data['% Cases Active'] = covid19_data['Active']/covid19_data['Confirmed']*100
 
+# Dividing in countries in groups based on Confirmed cases
+bin_values = [0, 15000, 30000, 100000, 500000, float('Inf')]
+bin_labels = ["< 15,000", "15,000 - 30,000", "30,000 - 100,000", "100,000 - 500,000", "500,000+"]
+cases_by_country = covid19_data.groupby('Country')[['Confirmed']].max().sort_values('Confirmed', ascending=False)
+cases_by_country['group'] = pd.cut(cases_by_country['Confirmed'], bins=bin_values , labels=bin_labels)
 
+# Plotting #Active Cases and % Active Cases
 
-# # Read Global data files for confirmed, recovered and deaths data.
-# confirmed_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-# recovered_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
-# deaths_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+#Plotting for single country: US
+countries = ['US']
+data = covid19_data.loc[countries]
+plot_data(data, ['Active', '% Cases Active'], 'Date', 'Country', 'Active Cases in '+str(countries))
 
-# df_confirmed = pd.read_csv(confirmed_url)
-# df_recovered = pd.read_csv(recovered_url)
-# df_deaths = pd.read_csv(deaths_url)
-
-# # Removing and renaming columns.
-# df_confirmed = df_confirmed.rename(columns={"Country/Region":"Country"}).drop(["Lat","Long"], axis="columns")
-# df_recovered = df_recovered.rename(columns={"Country/Region":"Country"}).drop(["Lat","Long"], axis="columns")
-# df_deaths = df_deaths.rename(columns={"Country/Region":"Country"}).drop(["Lat","Long"], axis="columns")
-
-# # Aggregating data at Country Level and dropping "Province/State" column
-# df_confirmed = df_confirmed.groupby("Country").sum()
-# df_recovered = df_recovered.groupby("Country").sum()
-# df_deaths = df_deaths.groupby("Country").sum()
-
-# # Pivoting down data
-# df_confirmed = df_confirmed.reset_index().melt(id_vars='Country', var_name="Date", value_name="Confirmed")
-# df_recovered = df_recovered.reset_index().melt(id_vars='Country', var_name="Date", value_name="Recovered")
-# df_deaths = df_deaths.reset_index().melt(id_vars='Country', var_name="Date", value_name="Deaths")
-
-# df_confirmed['Date'] = pd.to_datetime(df_confirmed['Date'])
-# df_recovered['Date'] = pd.to_datetime(df_recovered['Date'])
-# df_deaths['Date'] = pd.to_datetime(df_deaths['Date'])
-
-
+#Plotting for Bins "30,000 - 100,000", "100,000 - 500,000"
+for  val in ["30,000 - 100,000", "100,000 - 500,000"]:
+    countries = cases_by_country[cases_by_country['group'] == val].reset_index()['Country'].unique()
+    data = covid19_data.loc[countries]
+    plot_data(data, ['Active', '% Cases Active'], 'Date', 'Country', 'Active Cases in Countries with '+val+' Confirmed Cases')
